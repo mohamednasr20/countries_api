58985f7eae3134e14d1b1c58763f3edf9e980324
mohamednasr20 <mohamednasr8687@gmail.com>
2021-01-19 17:25:30 -0500

    adjust select input width

--- src/styles/SelectInputStyles.js
+++ src/styles/SelectInputStyles.js
@@ -2,7 +2,7 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    minWidth: 150,
+    minWidth: 200,
     [theme.breakpoints.down("sm")]: {
       marginTop: "1rem",
     },
