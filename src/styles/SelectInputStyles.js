2df9feba76faa5dc6a0e5c4cc27d6f961334fd7e
mohamednasr20 <mohamednasr8687@gmail.com>
2021-01-19 17:19:08 -0500

    select input bug

--- src/styles/SelectInputStyles.js
+++ src/styles/SelectInputStyles.js
@@ -2,7 +2,7 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: 150,
+    minWidth: 150,
     [theme.breakpoints.down("sm")]: {
       marginTop: "1rem",
     },
